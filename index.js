d07ad286312b03c17695b76eadfdc35bb8903bb0
Samuel Lord <sllord@icloud.com>
2020-07-11 18:36:00 -0500

    Initial commit
    Create my site.

---
+++ index.js
@@ -0,0 +1,87 @@
+const express = require('express')
+const readline = require('readline');
+const fs = require('fs');
+var request = require("request");
+var showdown  = require('showdown');
+const app = express()
+const port = 80 || 6969
+const sqlite3 = require('sqlite3').verbose();
+const pathResolver = require('path').resolve
+
+
+
+let tdb = new sqlite3.Database('testdb'); //a test database
+var trys = 0
+var adminsfdasfdsfds = false
+var loggedOut = true
+var filename = ""
+var head = ""
+
+filename = 'index.md'
+head = '<head><title>SLLORD.info - Main Page.</title></head>'
+fs.readFile('index.md', 'utf8', function (err,data) {
+    if (err) {
+        console.log("!!!ERROR!!!\nSomeone tried to access a MarkDown page,\n but for some odd reason we got an error reading the file!\nDetails:\n")
+        return console.log(err);
+    } else {
+        app.get('/', (req, res) => { 
+            console.log(`Converting markdown: \n ${data}\n`)
+            converter = new showdown.Converter(),
+            text = `${data}\n`,
+            body = converter.makeHtml(text);
+            console.log(`Converted: ${data}\n To: ${body}\n Now serving...`)
+            res.send(`<!DOCTYPE html><html>${head}<body>${body}</body></html>`)
+            console.log(`Somebody successfully went to ${req.originalUrl}`)
+        })
+    }
+});
+
+app.get('/apiproxy', (req,res) => {
+    var url = req.query.url
+    request({
+        uri: url,
+    }, function(error, response, body) {
+        res.send(body)
+    });
+})
+
+app.get('/picOfMe', (req,res) => {
+    res.sendFile(pathResolver('sllord.png'))
+})
+
+
+app.get('/admin', (req, res) => {
+    function tryAdmin() {
+        if (trys == 0) {
+            res.send("<h1>Admin panel, please login</h1>  <form action='/admin'><label for='key'>Key:</label><br><input type='text' id='key' name='key' value='Insert key here.'><br><input type='submit' value='Submit key'></form>")
+        } else{ 
+            res.send(`<h1>Admin panel, please login</h1> <p><b>Try again. Trys: ${trys}</b></p>  <form action='/admin'><label for='key'>Key:</label><br><input type='text' id='key' name='key' value='Insert key here.'><br><input type='submit' value='Submit key'></form>`)
+        }
+        trys++
+    }
+    console.log(req.originalUrl)
+    if (req.query.loggedIn == "true") {
+        if (adminsfdasfdsfds != true) {
+            res.send("<h1>GG, skid.</h1>");
+            console.log(`Someone tried to hack into the admin panel. IsAdmin=${adminsfdasfdsfds}`)            
+        } else {
+            res.send("Maybe coming soon. <hr> <a href='/admin?logout=true'>Logout</a>")
+        }
+    } else if (req.query.key == "Ki58FFuU46nOH8W" ) {
+        adminsfdasfdsfds = true
+        trys = 0
+        res.send(`<h1>Logged in as: ${req.query.key}.</h1> <hr> <a href='/admin?loggedIn=true'>Go to panel</a>`);
+    } else if (req.query.logout == "true") {
+        loggedOut = true
+        req.query.loggedIn = "false"
+        adminsfdasfdsfds = false
+        trys = 0
+        res.send("<h1>Logged out.</h1> <hr> <a href='/admin?loggedIn=false'>Log back in</a>");
+    } else {
+        tryAdmin()
+    }
+})
+
+app.listen(port, () => console.log(`SLLORD.info listening at http://localhost:${port}`))
+//After we end the server, close the database connection
+tdb.close();
---
+++ index.md
@@ -0,0 +1,27 @@
+# SLLORD.info
+## About me
+<img width="5%" src="/picOfMe"></img><br>
+Hello, my name is Samuel Lord, and this is my Node.JS testing ground and portfolio.  I am a fan of FOSS, VB.NET, Node.JS/JS, HTML, ROBLOX LUA, Panda3D (even if I am just beginning), and CSS. I have Autism and some signs of ADHD.
+
+## My *best* personal projects:
+***Note: Don't expect much active development on these, I just develop my personal projects for fun.***
+[SparksammyCOC,](https://github.com/sparksammy/SparksammyCOC)
+[GEMS,](https://github.com/sparksammy/GEMS)
+[RoboLangMRW,](https://github.com/sparksammy/RoboLangMRW)
+[Video2Resolve,](https://github.com/sparksammy/Video2Resolve)
+[IAteThis,](https://github.com/sparksammy/IAteThis)
+[Pacify,](https://github.com/sparksammy/Pacify)
+[SUsie,](https://github.com/sparksammy/SUsie)
+[FurcodeJS,](https://github.com/sparksammy/FurcodeJS)
+[NoteWords,](https://github.com/sparksammy/NoteWords)
+[SLLORD.info (this site),](https://github.com/sparksammy/sllord.info)
+[and, of course we can't forget about my main website, sparkSammy.com!](https://sparksammy.com)
+
+## Why do I do what I do?
+I program because I find it fun to create programs and to create solutions with said programs! Sometimes, I even program just for fun! Basically: I really like programming just to solve issues and have fun doing what I love!
+
+## Do you enjoy programming?
+Very much so, but sometimes I get quite frustrated at not being able to solve errors with the code, but it's OK.
+
+---
+Copyright Samuel Lord. Some rights reserved.
